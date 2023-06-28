3dbcc63d0578cd2e2005792ea26c16f413ca3297
David Sims <dsims37@gatech.edu>
2023-06-28 14:51:28 -0700

    initial commit

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("should render App", () => {
-    const component = render(App);
+    const component: MyPromise = render(App);
 
     expect(component).toBeTruthy();
 });
